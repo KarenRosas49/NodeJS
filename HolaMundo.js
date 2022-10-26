3772cc11bec1c6e62993888835a8d9f184639578
Karen Rosas <rosas.karen0926@gmail.com>
2022-10-26 09:38:16 -0500

    correccion

--- HolaMundo.js
+++ HolaMundo.js
@@ -1,3 +1,3 @@
-console.log("Hola Mundo);
+console.log("Hola Mundo");
 console.log(2 + 2);
-console.log("Hola a todos! Observen este numero: ", 5 + 18);
+console.log("Hola a todos! Observen este numero: ", 5 + 18); 
