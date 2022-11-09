4f27324f6fa2c68505471304d2d847055aceb1ba
Karen Rosas <rosas.karen0926@gmail.com>
2022-11-09 10:28:52 -0600

    practica final

--- ApiNodeJSExpress/src/index.js
+++ ApiNodeJSExpress/src/index.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-
-app.set('port', process.env.PORT || 3000);
-app.set('json spaces', 2);
-
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+var express = require('express');
+var app = express();
+const path = require('path');
 app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/html/index.html'));
+});
+
+
+app.post("/datos", function (req, res) {
     res.json(
         {
-            "Title": "Hola mundo"
+            "Nombre": req.body.nombre,
+            "Apellidos": req.body.apellidos,
+            "Direccion": req.body.direccion,
+            "Telefono": req.body.telefono
         }
     );
 });
 
-app.listen(app.get('port'),()=>{
+app.listen(3000, function() {
     console.log(`Server listenning on port ${app.get('port')}`);
 });
---
+++ ApiNodeJSExpress/src/index_1.txt
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json(
+        {
+            "Title": "Hola mundo"
+        }
+    );
+});
+
+app.listen(app.get('port'),()=>{
+    console.log(`Server listenning on port ${app.get('port')}`);
+});
