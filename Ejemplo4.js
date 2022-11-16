fa5d4b939f1377b308484ac99fc7a6471e20288c
Karen Rosas <rosas.karen0926@gmail.com>
2022-11-16 10:37:02 -0600

    modificaciones

--- Ejemplo4.js
+++ Ejemplo4.js
@@ -8,6 +8,9 @@ var json = '{"nombre": "Karen":, "edad":21, "nacionalidad":"mexicana"}';
 var obj = JSON.parse(json);console.log(obj.nombre);obj.nombre = "pedro";
 console.log(obj.nombre);
 
-var json = '{"nombre": "Manuel":, "edad":51, "nacionalidad":"mexicana", "hobies": ["ajedrez]}';
-var obj = JSON.parse(json);console.log(obj.nombre);obj.nombre = "pedro";
-console.log(obj.nombre);
+var json = '{"nombre": "Manuel":, "edad":51, "nacionalidad":"mexicana", "hobies": ["ajedrez","videojuegos","comics"]}';
+
+var obj = JSON.parse(json);
+console.log(obj.hobies[0]);
+obj.hobies[0] = "peliculas";
+console.log(obj.hobies[0]);
